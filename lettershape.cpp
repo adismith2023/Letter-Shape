0dae30875c2ead2094f8d171103aac91b170320c
Adrian Smith <147835901+adismith2023@users.noreply.github.com>
2023-12-10 19:42:00 +0000

    Add files via upload

---
+++ lettershape.cpp
@@ -0,0 +1,15 @@
+//#include <iostream>
+//
+//int main()
+//{
+//	char pattern[7] = { 'A' , 'B' , 'C' , 'D' , 'E' , 'F' , 'G' };
+//
+//	for (int i = 0; i < 7; i++)
+//	{
+//		for (int j = 0; j <= i; j++)
+//		{
+//			std::cout << std::endl;
+//		}
+//	}
+//	return 0;
+//}
